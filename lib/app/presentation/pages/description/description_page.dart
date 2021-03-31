89e98307712d5fb55586da64f282168356faeeef
roma7481 <roma7481@gmail.com>
2021-03-30 21:05:40 -0400

    adding collapse widget

--- lib/app/presentation/pages/description/description_page.dart
+++ lib/app/presentation/pages/description/description_page.dart
@@ -1,5 +1,7 @@
+import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 import 'package:numerology/app/constants/colors.dart';
+import 'package:numerology/app/constants/text_styles.dart';
 import 'package:numerology/app/presentation/common_widgets/castom_category_card.dart';
 import 'package:numerology/app/presentation/common_widgets/custom_card.dart';
 
@@ -57,11 +59,59 @@ class DescriptionPage extends StatelessWidget {
   }
 
   Widget _buildCard(String header, String content) {
-    return CustomCard(
-        child: CustomCategoryCard(
-      header: header,
-      content: content,
-    ));
+    if (content.length > 1000) {
+      return CustomCard(
+          child: ExpandablePanel(
+        theme: ExpandableThemeData(iconColor: Colors.white),
+        header: _buildHeader(header),
+        collapsed: _buildCardContent(content: content, isFolded: true),
+        expanded: _buildCardContent(
+          content: content,
+        ),
+      ));
+    } else {
+      return CustomCard(
+          child: CustomCategoryCard(
+        header: header,
+        content: content,
+      ));
+    }
+  }
+
+  Widget _buildHeader(String header) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(
+          top: 16.0,
+          left: 48.0,
+        ),
+        child: Text(
+          header,
+          style: descriptionHeaderStyle,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCardContent({String content, bool isFolded = false}) {
+    if (isFolded) {
+      return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(
+          content,
+          maxLines: 4,
+          overflow: TextOverflow.ellipsis,
+          style: descriptionContentStyle,
+        ),
+      );
+    }
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Text(
+        content,
+        style: descriptionContentStyle,
+      ),
+    );
   }
 
   SliverChildDelegate _buildList(Map<String, String> cards) {
