18c4412def855ff313474eab031b503e746e542a
roma7481 <roma7481@gmail.com>
2021-04-05 12:54:07 -0400

    compat page setup

--- lib/app/presentation/pages/description/compat_internal_page.dart
+++ lib/app/presentation/pages/description/compat_internal_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:numerology/app/constants/colors.dart';
 import 'package:numerology/app/constants/icon_path.dart';
-import 'package:numerology/app/presentation/pages/home/custom_raised_button.dart';
 
 class CompatInternalPage extends StatefulWidget {
   @override
@@ -10,6 +9,8 @@ class CompatInternalPage extends StatefulWidget {
 }
 
 class _CompatInternalPageState extends State<CompatInternalPage> {
+  var _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -34,25 +35,53 @@ class _CompatInternalPageState extends State<CompatInternalPage> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            CustomButton(
-              child: Container(
-                child: SvgPicture.asset(work, height: width * 0.2),
-              ),
-              onPressed: () {},
-            ),
-            CustomButton(
-              child: Container(
-                child: SvgPicture.asset(work, height: width * 0.2),
-              ),
-              onPressed: () {},
-            ),
-            CustomButton(
-              child: Container(
-                child: SvgPicture.asset(work, height: width * 0.2),
-              ),
-              onPressed: () {},
-            ),
+            _buildButton(width, 0),
+            _buildButton(width, 1),
+            _buildButton(width, 2),
           ],
+        ),
+      ),
+    );
+  }
+
+  Column _buildButton(double width, int index) {
+    return Column(
+      children: [
+        customButton(
+          child: Container(
+            child: SvgPicture.asset(work, height: width * 0.2),
+          ),
+          onPressed: () {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+        ),
+        Opacity(
+            opacity: index == _selectedIndex ? 1 : 0,
+            child: buildLine(width * 0.2)),
+      ],
+    );
+  }
+
+  Widget buildLine(double width) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16.0),
+      child: Container(
+        height: 3.0,
+        width: width,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  Widget customButton({Widget child, VoidCallback onPressed}) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GestureDetector(
+        onTap: onPressed,
+        child: Container(
+          child: child,
         ),
       ),
     );
