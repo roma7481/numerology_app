69e44c9fd6c0cd6a3bededd4ad28070d242308b0
roma7481 <roma7481@gmail.com>
2021-04-25 17:58:30 -0400

    adding banner

--- lib/app/presentation/pages/description/description_page.dart
+++ lib/app/presentation/pages/description/description_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:numerology/app/business_logic/services/ads/ad_service.dart';
 import 'package:numerology/app/business_logic/services/ads/native_admob_controller.dart';
 import 'package:numerology/app/business_logic/services/ads/show_native_ad.dart';
 import 'package:numerology/app/constants/colors.dart';
@@ -13,6 +15,29 @@ class DescriptionPage extends StatelessWidget {
   final List<CardData> data;
   final NativeAdmobController adController;
 
+  AdWidget adWidget;
+
+  final BannerAd myBanner = BannerAd(
+    adUnitId: realBannerAppId,
+    size: AdSize.banner,
+    request: AdRequest(),
+    listener: AdListener(
+      // Called when an ad is successfully received.
+      onAdLoaded: (Ad ad) => print('Ad loaded.'),
+      // Called when an ad request failed.
+      onAdFailedToLoad: (Ad ad, LoadAdError error) {
+        ad.dispose();
+        print('Ad failed to load: $error');
+      },
+      // Called when an ad opens an overlay that covers the screen.
+      onAdOpened: (Ad ad) => print('Ad opened.'),
+      // Called when an ad removes an overlay that covers the screen.
+      onAdClosed: (Ad ad) => print('Ad closed.'),
+      // Called when an ad is in the process of leaving the application.
+      onApplicationExit: (Ad ad) => print('Left application.'),
+    ),
+  );
+
   DescriptionPage(
     this.adController, {
     this.calculation = '',
@@ -22,6 +47,8 @@ class DescriptionPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    myBanner.load();
+    adWidget = AdWidget(ad: myBanner);
     return _buildPageContent(context);
   }
 
@@ -34,15 +61,29 @@ class DescriptionPage extends StatelessWidget {
   }
 
   Widget _buildContext(BuildContext context) {
+    var listHeight = MediaQuery.of(context).size.height - myBanner.size.height - AppBar().preferredSize.height - 20.0;
+
     return Container(
-      color: backgroundColor,
-      child: CustomScrollView(
-        slivers: [
-          _buildNumberIcon(context, calculation),
-          _buildList(data),
-        ],
-      ),
-    );
+        color: backgroundColor,
+        child: Column(
+          children: [
+            SizedBox(
+              height: listHeight,
+              child: CustomScrollView(
+                slivers: [
+                  _buildNumberIcon(context, calculation),
+                  _buildList(data),
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: adWidget,
+              width: myBanner.size.width.toDouble(),
+              height: myBanner.size.height.toDouble(),
+            ),
+          ],
+        ));
   }
 
   Widget _buildNumberIcon(BuildContext context, String calculation) {
