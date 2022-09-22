cca0e83868c0166978d6d1a0e6e81813d1688fc7
Alisa Mak <alisa.mak@yahoo.com>
2022-09-22 18:08:28 -0400

    Revert "more apps use only EN locale"
    This reverts commit 09ed16c7

--- lib/app/data/repository/OtherAppsRepository.dart
+++ lib/app/data/repository/OtherAppsRepository.dart
@@ -1,13 +1,14 @@
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:numerology/app/business_logic/globals/globals.dart';
 import 'package:numerology/app/data/models/OtherApp.dart';
 
 class OtherAppsRepository {
 
   Future<List<OtherApp>> getAll() async {
     Query<OtherApp> snapshot = FirebaseFirestore.instance
-        .collection("en/other_apps/${Platform.operatingSystem}")
+        .collection("${Globals.instance.localeCode}/other_apps/${Platform.operatingSystem}")
         .withConverter<OtherApp>(
       fromFirestore: (snapshots, _) => OtherApp.fromJson(snapshots.data()),
       toFirestore: (category, _) => category.toJson(),
