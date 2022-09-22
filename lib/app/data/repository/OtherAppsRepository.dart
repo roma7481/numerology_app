09ed16c7116caffc61d6af745088495b25be6933
Alisa Mak <alisa.mak@yahoo.com>
2022-09-22 16:23:00 -0400

    more apps use only EN locale

--- lib/app/data/repository/OtherAppsRepository.dart
+++ lib/app/data/repository/OtherAppsRepository.dart
@@ -1,14 +1,13 @@
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:numerology/app/business_logic/globals/globals.dart';
 import 'package:numerology/app/data/models/OtherApp.dart';
 
 class OtherAppsRepository {
 
   Future<List<OtherApp>> getAll() async {
     Query<OtherApp> snapshot = FirebaseFirestore.instance
-        .collection("${Globals.instance.localeCode}/other_apps/${Platform.operatingSystem}")
+        .collection("en/other_apps/${Platform.operatingSystem}")
         .withConverter<OtherApp>(
       fromFirestore: (snapshots, _) => OtherApp.fromJson(snapshots.data()),
       toFirestore: (category, _) => category.toJson(),
